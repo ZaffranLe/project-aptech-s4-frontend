60fbb0f84d63779ae0b6f134b4fa6d88ba7dc34e
unknown <biprono12@gmail.com>
2020-06-05 22:36:49 +0700

    add props children to layout

--- src/pages/admin-dashboard/index.js
+++ src/pages/admin-dashboard/index.js
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout/layout";
 import React from "react";
+import { Grid } from "semantic-ui-react";
 
 class AdminDashboard extends React.Component {
     constructor(props) {
@@ -8,7 +9,11 @@ class AdminDashboard extends React.Component {
 
     render() {
         return (
-
+            <Layout>
+                <Grid.Row>
+                    
+                </Grid.Row>
+            </Layout>
         );
     }
 }
