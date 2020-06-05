388da9fc069b1766e66996597e7e7f6d5be5eadf
unknown <biprono12@gmail.com>
2020-06-05 22:07:56 +0700

    Page folder

---
+++ src/pages/admin-dashboard/index.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/layout/layout";
+import React from "react";
+
+class AdminDashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default AdminDashboard;
