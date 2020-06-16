71b3214155735807a85e625c016d0faf7dc02e14
unknown <biprono12@gmail.com>
2020-06-16 23:25:05 +0700

    update product type page

--- app/routes/Apps/ProductTypes/ProductTypesList.js
+++ app/routes/Apps/ProductTypes/ProductTypesList.js
@@ -16,21 +16,77 @@ import {
     Alert,
     Table,
     Container,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
 } from "./../../../components";
 import { ProductTypeActions } from "../../../redux/_actions/ProductTypes/ProductTypesA";
-import { v1 } from "uuid";
+import { v1, v4 } from "uuid";
 import { HeaderMain } from "../../components/HeaderMain";
+import moment from "moment";
 
 class ModifyModal extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            Name: "",
+            Images: [],
+            formData: new FormData()
+        }
     }
 
+    componentDidMount() {
+        const { type } = this.props;
+        if (type) {
+            const images = [];
+            if (type["ListImage"].length > 0) {
+                type["ListImage"].map((img, idx) => {
+                    images.push({
+                        isDeleted: false,
+                        id: img["Id"],
+                        url: img["ImageUrl"]
+                    });
+                })
+            }
+            this.setState({
+                Name: type["ProductType"]["Name"],
+                Images: images
+            })
+        }
+    }
+
+    handlePreviewImages = (e) => {
+        const imagesPreview = [];
+        const formData = new FormData();
+        for (let file of e.target.files) {
+            const name = v4();
+            imagesPreview.push({
+                url: URL.createObjectURL(file),
+                name,
+            });
+            formData.append(name, file);
+        }
+        this.setState({
+            imagesPreview,
+            formData
+        });
+    };
+
     render() {
-        const { isOpen, type }= this.props;
+        const { isOpen, onClose }= this.props;
         return (
-            <Modal isOpen={isOpen}>
-                
+            <Modal isOpen={isOpen} toggle={onClose}>
+                <ModalHeader>
+                    Loại sản phẩm
+                </ModalHeader>
+                <ModalBody>
+
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary">Lưu</Button>
+                    <Button onClick={onClose}>Đóng</Button>
+                </ModalFooter>
             </Modal>
         );
     }
@@ -49,6 +105,20 @@ class ProductTypesList extends React.Component {
         this.props.dispatch(ProductTypeActions.getAllProductType());
     }
 
+    handleCloseModifyModal = () => {
+        this.setState({
+            modifyModal: false,
+            productType: "",
+        })
+    }
+
+    handleOpenModifyModal = (productType = "") => {
+        this.setState({
+            modifyModal: true,
+            productType,
+        })
+    }
+
     render() {
         const { productTypes } = this.props;
         const { productType, modifyModal } = this.state;
@@ -59,7 +129,7 @@ class ProductTypesList extends React.Component {
                         <HeaderMain title="Thể loại sản phẩm" className="mb-5 mt-4" />
                     </Col>
                     <Col lg={3} className="text-right mt-4">
-                        <Button color="primary">Tạo sản phẩm mới</Button>
+                        <Button color="primary" onClick={this.handleOpenModifyModal} >Tạo sản phẩm mới</Button>
                     </Col>
                 </Row>
                 <Row>
@@ -80,15 +150,15 @@ class ProductTypesList extends React.Component {
                                     <tbody>
                                         {productTypes.map((type, idx) => {
                                             return (
-                                                <tr key={type["Id"]}>
+                                                <tr key={type["ProductType"]["Id"]}>
                                                     <td>{idx + 1}</td>
                                                     <td>#Ảnh</td>
-                                                    <td>{type["Name"]}</td>
-                                                    <td>{type["CreatedAt"]}</td>
-                                                    <td>{type["UpdatedAt"]}</td>
+                                                    <td>{type["ProductType"]["Name"]}</td>
+                                                    <td>{moment(type["ProductType"]["CreatedAt"]).format("YYYY-MM-DD HH:mm:ss")}</td>
+                                                    <td>{moment(type["ProductType"]["UpdatedAt"]).format("YYYY-MM-DD HH:mm:ss")}</td>
                                                     <td>
-                                                        <Button color="yellow">Edit</Button>{" "}
-                                                        <Button color="red">Delete</Button>
+                                                        <Button color="yellow" onClick={() => this.handleOpenModifyModal(type)}>Edit</Button>{" "}
+                                                        <Button color="danger">Delete</Button>
                                                     </td>
                                                 </tr>
                                             );
@@ -101,7 +171,7 @@ class ProductTypesList extends React.Component {
                         </Container>
                     </Col>
                 </Row>
-                <ModifyModal key={productType["Id"] || v1()} productType={productType} isOpen={modifyModal} />
+                <ModifyModal key={productType["Id"] || v1()} type={productType} isOpen={modifyModal} onClose={this.handleCloseModifyModal} />
             </React.Fragment>
         );
     }
